5a4e553d2cf1a69bc21b3501701c9aa9fec86a2e
azmianshari <azmianshari123@gmail.com>
2024-11-13 15:03:49 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/p6/ui/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.p6.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.p6.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(Mahasiswa())
+    val uiState: StateFlow<Mahasiswa> = _uiState.asStateFlow()
+
+    fun saveDataMahasiswa(ls: MutableList<String>) {
+        _uiState.update { data ->
+            data.copy(
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2]
+            )
+        }
+    }
+}
