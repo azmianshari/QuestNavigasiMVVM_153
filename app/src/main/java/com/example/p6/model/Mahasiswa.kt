5213b45df3992fc507908eeb8dd8c8503dc5d0eb
azmianshari <azmianshari123@gmail.com>
2024-11-13 15:09:34 +0700

    menambahkan email dan noHp

--- app/src/main/java/com/example/p6/model/Mahasiswa.kt
+++ app/src/main/java/com/example/p6/model/Mahasiswa.kt
@@ -3,5 +3,7 @@ package com.example.p6.model
 data class Mahasiswa(
     val nama : String ="",
     val gender : String ="",
-    val alamat : String =""
+    val alamat : String ="",
+    val email : String ="",
+    val noHp : String =""
 )
