61bee9c0fbbcba95e27c415fe06396b9183d4cf2
azmianshari <azmianshari123@gmail.com>
2024-11-13 15:11:42 +0700

    menambahkan properti email dan noHp di viewmodel

--- app/src/main/java/com/example/p6/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/p6/ui/viewmodel/MahasiswaViewModel.kt
@@ -16,7 +16,9 @@ class MahasiswaViewModel : ViewModel() {
             data.copy(
                 nama = ls[0],
                 gender = ls[1],
-                alamat = ls[2]
+                alamat = ls[2],
+                email = ls[3],
+                noHp = ls[4]
             )
         }
     }
