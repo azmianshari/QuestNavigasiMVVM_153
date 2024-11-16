5fa50ffda3493e7276e9fcf18932aea470fd2e6d
azmianshari <azmianshari123@gmail.com>
2024-11-16 20:01:24 +0700

    Menambahkan nim pada dataclass

--- app/src/main/java/com/example/p6/model/Mahasiswa.kt
+++ app/src/main/java/com/example/p6/model/Mahasiswa.kt
@@ -2,6 +2,7 @@ package com.example.p6.model
 
 data class Mahasiswa(
     val nama : String ="",
+    val nim : String="",
     val gender : String ="",
     val alamat : String ="",
     val email : String ="",
