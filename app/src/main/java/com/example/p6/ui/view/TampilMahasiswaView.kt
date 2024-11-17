6e92e41a98bf44cd2767cee9561481e17a5c5c56
azmianshari <azmianshari123@gmail.com>
2024-11-17 13:30:49 +0700

    Mengupdate posisi tombol back nya

--- app/src/main/java/com/example/p6/ui/view/TampilMahasiswaView.kt
+++ app/src/main/java/com/example/p6/ui/view/TampilMahasiswaView.kt
@@ -1,61 +1,56 @@
 package com.example.p6.ui.view
 
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.navigation.NavHostController
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import com.example.p6.model.Mahasiswa
-
 
 @Composable
 fun TampilMahasiswaView(
     mhs: Mahasiswa,
     navController: NavHostController
 ) {
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Konten Data Mahasiswa
+        TampilData(
+            judul = "Nama", isinya = mhs.nama
+        )
+        TampilData(
+            judul = "Nim", isinya = mhs.nim
+        )
+        TampilData(
+            judul = "Jenis Kelamin", isinya = mhs.gender
+        )
+        TampilData(
+            judul = "Alamat", isinya = mhs.alamat
+        )
+        TampilData(
+            judul = "Email", isinya = mhs.email
+        )
+        TampilData(
+            judul = "NoHp", isinya = mhs.noHp
+        )
 
-        androidx.compose.material3.Button(
-            onClick = { navController.popBackStack() }
+        Spacer(modifier = Modifier.weight(1f))
+
+
+        Button(
+            onClick = { navController.popBackStack() },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
         ) {
             Text("Back")
         }
-
-        TampilData(
-            judul = "Nama",
-            isinya = mhs.nama
-        )
-        TampilData(
-            judul = "Nim",
-            isinya = mhs.nim
-        )
-        TampilData(
-            judul = "Jenis Kelamin",
-            isinya = mhs.gender
-        )
-        TampilData(
-            judul = "Alamat",
-            isinya = mhs.alamat
-        )
-        TampilData(
-            judul = "Email",
-            isinya = mhs.email
-        )
-        TampilData(
-            judul = "NoHp",
-            isinya = mhs.noHp
-        )
     }
-}
-
-
-@Composable
-fun Button(onClick: Any, content: @Composable () -> Unit) {
-
 }
 
 @Composable
